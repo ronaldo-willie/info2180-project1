2f14a3f987d07f222337fb9c8abf20d868963f62
Ronaldo Willie <ronaldo_kc@outlook.com>
2020-11-01 16:32:03 -0500

    updated index.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+"use strict";
+
+window.onload = function {Email()};
+/*getElementsByClassName("btn").onclick = function() {Email()};*/
+getElementsByClassName("btn").addEventListener("click", function(){Email()});
+
+function Email(formData){
+	let newEmail = formData.email.value;
+	var messageElement = document.querySelector('.message');
+	if (newEmail){
+		messageElement.innerHtml = `Your email address <${newEmail}> has been added to our mailing list`;
+		return false;
+	}else{
+		messageElement.innerHtml = "Email must be filled out, please enter a valid email address";
+		return false;
+	}
+}
